3399c6a8fdc214718300f12115017a7b9396c534
Swarnalathaa <latha.swarna@broadcom.com>
2023-08-01 17:40:38 +0200

    fix: github issue

--- src/app/pages/common/dup/upsert-dup/upsert-dup.component.ts
+++ src/app/pages/common/dup/upsert-dup/upsert-dup.component.ts
@@ -365,6 +365,12 @@ export class UpsertDupComponent implements OnInit {
   }
   dateToString(date) {
     if (date) {
+      if (date.month<10) {
+        date.month = '0'+ date.month
+      }
+      if (date.day < 10) {
+        date.day = '0' + date.day
+      }
       const dateString =  date.year + '-' + date.month + '-' + date.day;
       return new Date(dateString).toISOString();
     } else {
